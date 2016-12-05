38a7adca4e8040fb5e3297bc018324fea5ddf317
José Roa <jg.roac@gmail.com>
2016-12-05 18:58:34 -0300

    Agregados Nodos, Mejorado los punteros

--- pruebas/sintactico/ejemplo.mc
+++ pruebas/sintactico/ejemplo.mc
@@ -12,6 +12,7 @@ int main()
     int var1, var2, *var3;
     gets(var1);
     gets(var2);
+    var2 = *var1 * *var1;
     int salida;
     var3=&salida;
     *salida = suma (var1, var2);
--- src/ast/NodoFor.java
+++ src/ast/NodoFor.java
@@ -1,7 +1,25 @@
 package ast;
 
 public class NodoFor extends NodoBase {
-	public NodoFor() {
-		
+	private NodoBase lista_declaracion;
+	private NodoBase exp;
+	private NodoBase declaracion;
+	
+	public NodoFor(NodoBase lista_declaracion, NodoBase exp, NodoBase declaracion) {
+		this.lista_declaracion = lista_declaracion;
+		this.exp = exp;
+		this.declaracion = declaracion;
+	}
+	
+	public NodoBase getListaDeclaracion(){
+		return this.lista_declaracion;
+	}
+	
+	public NodoBase getExpresion(){
+		return this.exp;
+	}
+	
+	public NodoBase getDeclaracion(){
+		return this.declaracion;
 	}
 }
