81fd82fadd11c11f397b8d8f69f2137c3faa5798
José Roa <jg.roac@gmail.com>
2016-12-05 18:59:21 -0300

    Agregados Nodo Cuerpo, NodoLeer

--- src/ast/NodoCuerpo.java
+++ src/ast/NodoCuerpo.java
@@ -2,10 +2,17 @@ package ast;
 
 public class NodoCuerpo extends NodoBase {
 	
-	private NodoBase variables;
 	private NodoBase sentencias;
 
-	public NodoCuerpo() {
-		
+	public NodoCuerpo(NodoBase sentencias) {
+		this.sentencias = sentencias;
+	}
+	
+	public NodoBase getSentencias() {
+		return this.sentencias;
+	}
+	
+	public void setSentencias (NodoBase sentencias){
+		this.sentencias = sentencias;
 	}
 }
--- src/ast/NodoLeer.java
+++ src/ast/NodoLeer.java
@@ -2,19 +2,10 @@ package ast;
 
 public class NodoLeer extends NodoBase {
 	private String id;
-	private NodoBase array;
-	private NodoBase posArray;
 
 	public NodoLeer(String identificador) {
 		super();
 		this.id = identificador;
-	}
-
-	public NodoLeer(NodoBase array) {
-		super();
-		this.id = ((NodoIdentificador) array ).getNombre();
-		this.array = array;
-		this.posArray = ((NodoIdentificador)array ).getExpresion();
 	}
 
 	public String getIdentificador() {
